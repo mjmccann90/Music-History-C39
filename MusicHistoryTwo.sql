b7840a5eb26f7abb767955b01d40e1ed850f1766
Mark McCann <mjmccann12@me.com>
2020-06-03 13:10:30 -0500

    finished live code

---
+++ MusicHistoryTwo.sql
@@ -0,0 +1,26 @@
+﻿----SELECT
+--    Id,
+--    Title,
+--    ReleaseDate
+--FROM Song;
+
+--SELECT 
+--Id,
+--ArtistName
+--from Artish
+
+
+
+
+SELECT 
+  song.Title,
+  song.RelatedData,
+  song.GenreId,
+  song.ArtistId, 
+  artist.ArtistName
+FROM Song song 
+JOIN Artist artist ON song.ArtistId = artist.id
+
+SELECT Id, Title, GenreId, genre.Label
+FROM Album album
+JOIN Genre genre ON album.GenreId = genre.Id
